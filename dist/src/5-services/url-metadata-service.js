0ab271c97da98dd2107778b609054d5d416ecc88
ShlomiMix <146866687+ShlomiMix@users.noreply.github.com>
2024-08-22 12:18:52 +0300

    third commit

--- dist/src/5-services/url-metadata-service.js
+++ dist/src/5-services/url-metadata-service.js
@@ -27,49 +27,49 @@ class UrlMetadataService {
             return url;
         }
     }
-    // Function to fetch metadata for a single URL
-    //   private async fetchMetadata(url: string) {
-    //     // if (
-    //     //   !validator.isURL(url, {
-    //     //     protocols: ["http", "https"],
-    //     //     require_protocol: true,
-    //     //   })
-    //     // ) {
-    //     //   throw new Error(`Invalid URL: ${url}`);
-    //     // }
-    //     if (!validator.isURL(url, { protocols: ["http", "https"], require_protocol: true })) {
-    //         throw { status: 400, message: `Invalid URL: ${url}` };
-    //     }
-    //     try {
-    //       const sanitizedUrl = this.sanitizeUrl(url);
-    //       console.log("sanitizedUrl", sanitizedUrl);
-    //       // Debugging output
-    //       const { data } = await axios.get(url);
-    //       const dom = new JSDOM(data);
-    //       const title = dom.window.document.querySelector("title")?.textContent;
-    //       const description = dom.window.document
-    //         .querySelector('meta[name="description"]')
-    //         ?.getAttribute("content");
-    //       const image = dom.window.document
-    //         .querySelector('meta[property="og:image"]')
-    //         ?.getAttribute("content");
-    //       // Throw an error if any of these fields are missing
-    //       if (!title) throw new Error(`Missing title for URL: ${url}`);
-    //       if (!description) throw new Error(`Missing description for URL: ${url}`);
-    //       if (!image) throw new Error(`Missing image for URL: ${url}`);
-    //       return { url, title, description, image };
-    //     } catch (error:any) {
-    //       console.error(`Error fetching metadata for URL: ${url}`, error); // Additional error logging
-    //       throw new Error(
-    //         `Error fetching metadata for URL: ${url}. ${error.message}`
-    //       );
-    //     }
-    //   }
+    //   Function to fetch metadata for a single URL
+    fetchMetadata(url) {
+        return __awaiter(this, void 0, void 0, function* () {
+            var _a, _b, _c;
+            if (!validator_1.default.isURL(url, {
+                protocols: ["http", "https"],
+                require_protocol: true,
+            })) {
+                throw new Error(`Invalid URL: ${url}`);
+            }
+            try {
+                const sanitizedUrl = this.sanitizeUrl(url);
+                console.log("sanitizedUrl", sanitizedUrl);
+                // Debugging output
+                const { data } = yield axios_1.default.get(url);
+                const dom = new jsdom_1.JSDOM(data);
+                const title = (_a = dom.window.document.querySelector("title")) === null || _a === void 0 ? void 0 : _a.textContent;
+                const description = (_b = dom.window.document
+                    .querySelector('meta[name="description"]')) === null || _b === void 0 ? void 0 : _b.getAttribute("content");
+                const image = (_c = dom.window.document
+                    .querySelector('meta[property="og:image"]')) === null || _c === void 0 ? void 0 : _c.getAttribute("content");
+                // Throw an error if any of these fields are missing
+                if (!title)
+                    throw new Error(`Missing title for URL: ${url}`);
+                if (!description)
+                    throw new Error(`Missing description for URL: ${url}`);
+                if (!image)
+                    throw new Error(`Missing image for URL: ${url}`);
+                return { url, title, description, image };
+            }
+            catch (error) {
+                console.error(`Error fetching metadata for URL: ${url}`, error); // Additional error logging
+                throw new Error(`Error fetching metadata for URL: ${url}. ${error.message}`);
+            }
+        });
+    }
     // Function to add new metadata
     addMetadata(urls) {
         return __awaiter(this, void 0, void 0, function* () {
+            // Access the array of URLs from the input object
+            const urlList = urls.url;
             try {
-                const metadataPromises = urls.map((url) => this.fetchMetadata(url));
+                const metadataPromises = urlList === null || urlList === void 0 ? void 0 : urlList.map((url) => this.fetchMetadata(url));
                 const metadataArray = yield Promise.all(metadataPromises);
                 const savedMetadata = yield url_metadata_model_1.UrlMetadataModel.insertMany(metadataArray);
                 return savedMetadata;
@@ -79,49 +79,10 @@ class UrlMetadataService {
             }
         });
     }
-    fetchMetadata(url) {
+    getAllMetadata() {
         return __awaiter(this, void 0, void 0, function* () {
-            var _a, _b, _c;
-            if (!validator_1.default.isURL(url, {
-                protocols: ["http", "https"],
-                require_protocol: true,
-            })) {
-                throw { status: 400, message: `Invalid URL: ${url}` };
-            }
-            try {
-                const sanitizedUrl = this.sanitizeUrl(url);
-                console.log("sanitizedUrl", sanitizedUrl);
-                const { data } = yield axios_1.default.get(sanitizedUrl);
-                const dom = new jsdom_1.JSDOM(data);
-                const title = ((_a = dom.window.document.querySelector("title")) === null || _a === void 0 ? void 0 : _a.textContent) || "";
-                const description = ((_b = dom.window.document
-                    .querySelector('meta[name="description"]')) === null || _b === void 0 ? void 0 : _b.getAttribute("content")) || "";
-                const image = ((_c = dom.window.document
-                    .querySelector('meta[property="og:image"]')) === null || _c === void 0 ? void 0 : _c.getAttribute("content")) || "";
-                // Throw an error if any of these fields are missing
-                if (!title)
-                    throw { status: 400, message: `Missing title for URL: ${url}` };
-                if (!description)
-                    throw { status: 400, message: `Missing description for URL: ${url}` };
-                if (!image)
-                    throw { status: 400, message: `Missing image for URL: ${url}` };
-                return { url, title, description, image };
-            }
-            catch (error) {
-                console.error(`Error fetching metadata for URL: ${url}`, error);
-                if (error.response) {
-                    throw {
-                        status: error.response.status,
-                        message: `Error fetching metadata for URL: ${url}. ${error.message}`,
-                    };
-                }
-                else {
-                    throw {
-                        status: 500,
-                        message: `Unknown error fetching metadata for URL: ${url}.`,
-                    };
-                }
-            }
+            const metadata = yield url_metadata_model_1.UrlMetadataModel.find().exec();
+            return metadata;
         });
     }
 }
--- dist/src/6-controllers/url-metadata-controller.js
+++ dist/src/6-controllers/url-metadata-controller.js
@@ -16,7 +16,6 @@ exports.urlMetadataRouter = void 0;
 const express_1 = __importDefault(require("express"));
 const url_metadata_service_1 = require("../5-services/url-metadata-service");
 const enums_1 = require("../3-models/enums");
-const csurf_1 = __importDefault(require("csurf")); // Add this import
 const helmet_1 = __importDefault(require("helmet")); // Add this import
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 // Data controller:
@@ -25,13 +24,13 @@ class UrlMetadataController {
     constructor() {
         // Create a router object for listening to HTTP requests:
         this.router = express_1.default.Router();
-        this.csrfProtection = (0, csurf_1.default)({
-            cookie: {
-                httpOnly: true,
-                secure: process.env.ENVIRONMENT === "production", // Use secure cookies in production
-                sameSite: "strict", // Adjust as needed
-            },
-        }); // CSRF protection middleware
+        //   private csrfProtection = csrf({
+        //     cookie: {
+        //       httpOnly: true,
+        //       secure: process.env.ENVIRONMENT === "production", // Use secure cookies in production
+        //       sameSite: "strict", // Adjust as needed
+        //     },
+        //   }); // CSRF protection middleware
         this.helmetMiddleware = (0, helmet_1.default)(); // Helmet middleware for security headers
         this.registerRoutes();
     }
@@ -41,6 +40,7 @@ class UrlMetadataController {
         this.router.use((0, cookie_parser_1.default)()); // Ensure cookieParser is used before csrfProtection
         // this.router.use(this.csrfProtection);
         this.router.get("/csrf-token", this.getCsrfToken);
+        this.router.get("/metadata", this.getAllMetadata);
         this.router.post("/fetch-metadata", this.addMetadata);
     }
     getCsrfToken(request, response, next) {
@@ -53,6 +53,17 @@ class UrlMetadataController {
                 const urls = request === null || request === void 0 ? void 0 : request.body;
                 const addedMetadata = yield url_metadata_service_1.urlMetadataService.addMetadata(urls);
                 response.status(enums_1.StatusCode.Created).json(addedMetadata);
+            }
+            catch (err) {
+                next(err);
+            }
+        });
+    }
+    getAllMetadata(request, response, next) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const allMetadata = yield url_metadata_service_1.urlMetadataService.getAllMetadata();
+                response.json(allMetadata);
             }
             catch (err) {
                 next(err);
